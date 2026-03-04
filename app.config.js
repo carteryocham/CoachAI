cb5715570d7525eb4c9130ba2ba364014b2bc545
Carter Yocham <carteryocham@Carters-MacBook-Pro.local>
2026-03-04 11:23:42 -0700

    Add EAS config and app icon setup

--- app.config.js
+++ app.config.js
@@ -1,13 +1,29 @@
-// app.config.js
 import 'dotenv/config';
 
 export default {
   expo: {
     name: 'CoachAI',
     slug: 'coachai',
-    // add/keep any other config you use here (icon, bundle id, etc.)
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#1C1C1E',
+    },
+    ios: {
+      supportsTablet: false,
+      bundleIdentifier: 'com.carteryocham.coachai',
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
+    },
     extra: {
       OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+      eas: {
+        projectId: '429ba28d-3c11-4f56-b906-d516065f6b96',
+      },
     },
   },
 };
